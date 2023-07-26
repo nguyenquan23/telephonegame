684044730a50944ac068c8550c815c0ddb3ff26c
qnguyen <nguyenquan.212002@gmail.com>
2023-07-26 17:43:48 +0700

    h

--- src/main/java/com/example/thetelephoneappbe/ThetelephoneappbeApplication.java
+++ src/main/java/com/example/thetelephoneappbe/ThetelephoneappbeApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 
+@SpringBootApplication
 public class ThetelephoneappbeApplication {
     public static void main(String[] args) {
         SpringApplication.run(ThetelephoneappbeApplication.class, args);
--- src/main/java/com/example/thetelephoneappbe/service/impl/RoomServiceImpl.java
+++ src/main/java/com/example/thetelephoneappbe/service/impl/RoomServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @Service
 public class RoomServiceImpl implements RoomService {
+    @Autowired
     private RoomRepository roomRepository;
 
     @Autowired
